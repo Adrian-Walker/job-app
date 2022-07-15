b172df2263b759271f4cb8fadf2d46adaf12b165
Adrian-Walker <awalker3365@gmail.com>
2022-07-14 23:52:18 -0400

    register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,14 @@
+import { initial } from "lodash";
 import React, { useState, useEffect } from "react";
-// import { Logo } from "..components/";
 import Wrapper from "../assets/wrappers/RegisterPage";
+import Logo from "../components/Logo";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 function Register() {
   const handleChange = (e) => {
@@ -9,12 +17,31 @@ function Register() {
 
   const onSubmit = (e) => {
     e.preventDefault(e.target);
+    console.log(e.target);
   };
-  
+
+  const [values, setValues] = useState(initialState);
+
   return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
-        fuck
+        <Logo />
+        <h3>Login</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            onChange={handleChange}
+            className="form-input"
+          ></input>
+        </div>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
       </form>
     </Wrapper>
   );
